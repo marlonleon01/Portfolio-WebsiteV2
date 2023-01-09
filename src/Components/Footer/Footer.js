898a9ee2389c36fc52d513beb628c0ef97753cf6
Marlon Leon <ghostveene@gmail.com>
2023-01-09 12:53:14 -0500

    added possible icons for later

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,19 @@
 import "./Footer.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
 
 export function Footer() {
     return (
-        <div>Footer</div>
+        <div></div>
+        // <div>
+        //     {/* Add real website links */}
+        //     <a href="" target="_blank" rel="noopener noreferrer">
+        //         <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+        //     </a>
+        //     <a href="https://github.com/marlonleon01" target="_blank" rel="noopener noreferrer">
+        //         <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon>
+        //     </a>
+        // </div>
     )
 }
