d6a764fa706dddd75ac13af4352b5c251c861ba6
Marlon Leon <ghostveene@gmail.com>
2023-01-09 12:31:43 -0500

    finished with header might need to change classes and locations

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,8 @@
 import "./Header.css"
+import Maya from "../../imgs/Maya photo.jpg"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
 
 export function Header() {
     return (
@@ -7,6 +11,18 @@ export function Header() {
             <h5>Hello I'm</h5>
             <h1>Marlon Leon</h1>
             <h5 className="text-light">Front-End Developer</h5>
+            <div className="me">
+                <img src={Maya} alt="me"></img>
+            </div>
+            <div className="header_socials">
+                {/* Add real website links */}
+                <a href="" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                </a>
+                <a href="https://github.com/marlonleon01" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon>
+                </a>
+            </div>
         </header>
     ) 
 }
