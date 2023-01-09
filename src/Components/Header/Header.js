b82b9815411cb1bd3dd5c21fa795bd65ea8ba644
Marlon Leon <ghostveene@gmail.com>
2023-01-09 14:52:44 -0500

    changed className to header-container

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import Maya from "../../imgs/Maya photo.jpg"
 export function Header() {
     return (
         <header>
-            <div className="container header_container">
+            <div className="container header-container">
                 <h5>Hello I'm</h5>
                 <h1>Marlon Leon</h1>
                 <h5 className="text-light">Front-End Developer</h5>
