4cab9a3709241c3654f248fd11e03547f9f6ccf1
Marlon Leon <ghostveene@gmail.com>
2023-01-09 12:11:01 -0500

    added text missing img and icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,11 @@ import "./Header.css"
 
 export function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header_container"></div>
+            <h5>Hello I'm</h5>
+            <h1>Marlon Leon</h1>
+            <h5 className="text-light">Front-End Developer</h5>
+        </header>
     ) 
 }
