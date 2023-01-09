15616e20b95c47737e89a79237108b04686353f1
Marlon Leon <ghostveene@gmail.com>
2023-01-09 13:39:13 -0500

    deleted experiences it will be place inside about

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {Navbar} from "./Components/Navbar/Navbar"
 import {Header} from "./Components/Header/Header"
 import {About} from "./Components/About/About"
-import {Experience} from "./Components/Experience/Experience"
 import {Portfolio} from "./Components/Portfolio/Portfolio"
 import {Contact} from "./Components/Contact/Contact"
 import {Footer} from "./Components/Footer/Footer"
@@ -12,7 +11,6 @@ export function App() {
             <Navbar />
             <Header />
             <About />
-            <Experience />
             <Portfolio />
             <Contact />
             <Footer />
