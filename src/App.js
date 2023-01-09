cfd95ae77a0e9f5f65e456e43e52488866651f86
Marlon Leon <ghostveene@gmail.com>
2023-01-09 11:00:03 -0500

    imported all components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import {Navbar} from "./Components/Navbar/Navbar"
+import {Header} from "./Components/Header/Header"
+import {About} from "./Components/About/About"
+import {Experience} from "./Components/Experience/Experience"
+import {Portfolio} from "./Components/Portfolio/Portfolio"
+import {Contact} from "./Components/Contact/Contact"
+import {Footer} from "./Components/Footer/Footer"
+
 export function App() {
-    
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <About />
+            <Experience />
+            <Portfolio />
+            <Contact />
+            <Footer />
+        </div>
+    )
 }
