48262c9c6c1bef138e141a12b4119461a2d7b97b
Marlon Leon <ghostveene@gmail.com>
2023-01-09 13:39:32 -0500

    added icons and text

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,29 @@
 import "./Navbar.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faHouse} from "@fortawesome/free-solid-svg-icons"
+import {faUser} from "@fortawesome/free-solid-svg-icons"
+import {faBook} from "@fortawesome/free-solid-svg-icons"
+import {faAddressBook} from "@fortawesome/free-solid-svg-icons"
 
 export function Navbar() {
     return (
-        <div></div>
+        <nav className="navbar">
+            <a href="#">
+                <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
+                <span className="navbar-text">Home</span>
+            </a>
+            <a href="#about">
+                <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                <span className="navbar-text">About</span>
+            </a>
+            <a href="#portfolio">
+                <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
+                <span className="navbar-text">Portfolio</span>
+            </a>
+            <a href="#contact">
+                <FontAwesomeIcon icon={faAddressBook}></FontAwesomeIcon>
+                <span className="navbar-text">Contact</span>
+            </a>
+        </nav>
     )
 }
