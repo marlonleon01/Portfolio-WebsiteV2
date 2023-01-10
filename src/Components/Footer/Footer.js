c56daebc80f58ba501ebc9a086576c1a4665c3de
Marlon Leon <ghostveene@gmail.com>
2023-01-10 18:51:32 -0500

    added elements missing styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,27 @@
 import "./Footer.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
-import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
+import {faSquareFacebook} from "@fortawesome/free-brands-svg-icons"
+import {faInstagramSquare} from "@fortawesome/free-brands-svg-icons"
 
 export function Footer() {
     return (
-        <div></div>
-        // <div>
-        //     {/* Add real website links */}
-        //     <a href="" target="_blank" rel="noopener noreferrer">
-        //         <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-        //     </a>
-        //     <a href="https://github.com/marlonleon01" target="_blank" rel="noopener noreferrer">
-        //         <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon>
-        //     </a>
-        // </div>
+        <footer>
+            <a href="#" className="footer-logo">Marlon</a>
+            <ul className="links">
+                <li><a href="#navbar">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+            </ul>
+            <div className="footer-socials">
+                <a href="https://www.facebook.com/marlon.leon.710" target="_blank" 
+                rel="noopener noreferrer" className="anchor-facebook">
+                    <FontAwesomeIcon icon={faSquareFacebook} className="facebook-icon"></FontAwesomeIcon>
+                </a>
+                <a href="https://www.instagram.com/marlon.z32/" target="_blank" 
+                rel="noopener noreferrer" className="anchor-instagram">
+                    <FontAwesomeIcon icon={faInstagramSquare} className="instagram-icon"></FontAwesomeIcon>
+                </a>
+            </div> 
+        </footer>
     )
 }
