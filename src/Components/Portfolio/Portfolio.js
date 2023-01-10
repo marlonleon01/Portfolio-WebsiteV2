b36d3c3c4974222117f612f6fdc5ac990a18d7a8
Marlon Leon <ghostveene@gmail.com>
2023-01-10 11:32:01 -0500

    created array and mapped through it to display six projects

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,97 +2,77 @@ import "./Portfolio.css"
 
 import Maya from "../../imgs/Maya photo.jpg"
 
+const dataPortfolio = [
+    {
+        id: 1,
+        image: Maya,
+        title: "React Info Site",
+        github: "https://github.com/marlonleon01/React-Info-Site",
+        liveView: "https://github.com/marlonleon01/React-Info-Site"
+    },
+    {
+        id: 2,
+        image: Maya,
+        title: "React Info Site",
+        github: "https://github.com/marlonleon01/React-Info-Site",
+        liveView: "https://github.com/marlonleon01/React-Info-Site"
+    },
+    {
+        id: 3,
+        image: Maya,
+        title: "React Info Site",
+        github: "https://github.com/marlonleon01/React-Info-Site",
+        liveView: "https://github.com/marlonleon01/React-Info-Site"
+    },
+    {
+        id: 4,
+        image: Maya,
+        title: "React Info Site",
+        github: "https://github.com/marlonleon01/React-Info-Site",
+        liveView: "https://github.com/marlonleon01/React-Info-Site"
+    },
+    {
+        id: 5,
+        image: Maya,
+        title: "React Info Site",
+        github: "https://github.com/marlonleon01/React-Info-Site",
+        liveView: "https://github.com/marlonleon01/React-Info-Site"
+    },
+    {
+        id: 6,
+        image: Maya,
+        title: "React Info Site",
+        github: "https://github.com/marlonleon01/React-Info-Site",
+        liveView: "https://github.com/marlonleon01/React-Info-Site"
+    },
+]
+
 export function Portfolio() {
     return (
         <section id="portfolio">
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
             <div className="container portfolio-container">
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={Maya} alt="React Info Site"></img>
-                    </div>
-                    <h3>React Info Site</h3>
-                    <div className="portfolio-item-btns">
-                    <a href="https://github.com/marlonleon01/React-Info-Site" className="btn" target="_blank" rel="noopener noreferrer">
-                        Github
-                    </a>
-                    <a href="https://github.com/marlonleon01/React-Info-Site" className="btn btn-primary">
-                        Live View
-                    </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={Maya} alt="React Info Site"></img>
-                    </div>
-                    <h3>React Info Site</h3>
-                    <div className="portfolio-item-btns">
-                    <a href="https://github.com/marlonleon01/React-Info-Site" className="btn" target="_blank" rel="noopener noreferrer">
-                        Github
-                    </a>
-                    <a href="https://github.com/marlonleon01/React-Info-Site" className="btn btn-primary">
-                        Live View
-                    </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={Maya} alt="React Info Site"></img>
-                    </div>
-                    <h3>React Info Site</h3>
-                    <div className="portfolio-item-btns">
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn" target="_blank" rel="noopener noreferrer">
-                            Github
-                        </a>
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn btn-primary">
-                            Live View
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={Maya} alt="React Info Site"></img>
-                    </div>
-                    <h3>React Info Site</h3>
-                    <div className="portfolio-item-btns">
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn" target="_blank" rel="noopener noreferrer">
-                            Github
-                        </a>
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn btn-primary">
-                            Live View
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={Maya} alt="React Info Site"></img>
-                    </div>
-                    <h3>React Info Site</h3>
-                    <div className="portfolio-item-btns">
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn" target="_blank" rel="noopener noreferrer">
-                            Github
-                        </a>
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn btn-primary">
-                            Live View
-                        </a>
-                    </div>
-                </article>
-                <article className="portfolio-item">
-                    <div className="portfolio-item-image">
-                        <img src={Maya} alt="React Info Site"></img>
-                    </div>
-                    <h3>React Info Site</h3>
-                    <div className="portfolio-item-btns">
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn" target="_blank" rel="noopener noreferrer">
-                            Github
-                        </a>
-                        <a href="https://github.com/marlonleon01/React-Info-Site" className="btn btn-primary">
-                            Live View
-                        </a>
-                    </div>
-                </article>
-                
+                {
+                    dataPortfolio.map(({id, image, title, github, liveView}) => {
+                        return (
+                            <article key={id} className="portfolio-item">
+                                <div className="portfolio-item-image">
+                                    <img src={image} alt={title}></img>
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio-item-btns">
+                                <a href={github} className="btn" target="_blank" rel="noopener noreferrer">
+                                    Github
+                                </a>
+                                <a href={liveView} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                                    Live View
+                                </a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
