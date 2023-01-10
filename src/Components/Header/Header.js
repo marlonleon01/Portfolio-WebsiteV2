4a6eb545dcd7e7fe82fdccabece5f2b73e3bf2da
Marlon Leon <ghostveene@gmail.com>
2023-01-10 06:24:42 -0500

    added divs to create effect

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,8 +8,31 @@ export function Header() {
                 <h5>Hello I'm</h5>
                 <h1>Marlon Leon</h1>
                 <h5 className="text-light">Front-End Developer</h5>
-                <div className="main-img">
-                    <img src={Maya} alt="me"></img>
+                <div className="card-group">
+                    <div className="big-img">
+                        <img src={Maya} alt="me"></img>
+                    </div>
+                    <div className="big-img">
+
+                    </div>
+                    <div className="big-img">
+                        
+                    </div>
+                    <div className="big-img">
+                        
+                    </div>
+                    <div className="small-img">
+                        
+                    </div>
+                    <div className="small-img">
+                        
+                    </div>
+                    <div className="small-img">
+                        
+                    </div>
+                    <div className="small-img">
+                        
+                    </div>
                 </div>
             </div>
         </header>
