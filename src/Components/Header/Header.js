4263cd65f9688a4fd59b3a67f767b818637be341
Marlon Leon <ghostveene@gmail.com>
2023-01-10 07:49:50 -0500

    reordered divs for img

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,28 +10,31 @@ export function Header() {
                 <h5 className="text-light">Front-End Developer</h5>
                 <div className="img-container">
                     <div className="img-group">
+                        <div className="small-img img">
+                            
+                        </div>
                         <div className="big-img img">
                             {/* <img src={Maya} alt="me"></img> */}
+                        </div>
+                        
+                        <div className="small-img img">
+                            
                         </div>
                         <div className="big-img img">
 
                         </div>
+                        
+                        <div className="small-img img">
+                            
+                        </div>
                         <div className="big-img img">
                             
                         </div>
+                        
+                        <div className="small-img img">
+                            
+                        </div>
                         <div className="big-img img">
-                            
-                        </div>
-                        <div className="small-img img">
-                            
-                        </div>
-                        <div className="small-img img">
-                            
-                        </div>
-                        <div className="small-img img">
-                            
-                        </div>
-                        <div className="small-img img">
                             
                         </div>
                     </div>
