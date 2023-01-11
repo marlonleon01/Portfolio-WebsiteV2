9ad8960b265851a4305ca1478465382bb459686d
Marlon Leon <ghostveene@gmail.com>
2023-01-11 06:49:28 -0500

    added more icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,24 +2,32 @@ import "./Footer.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faSquareFacebook} from "@fortawesome/free-brands-svg-icons"
 import {faInstagramSquare} from "@fortawesome/free-brands-svg-icons"
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
 
 export function Footer() {
     return (
         <footer>
-            <a href="#" className="footer-logo">Marlon</a>
+            <a href="#" className="footer-name">Marlon Leon</a>
             <ul className="links">
                 <li><a href="#navbar">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#portfolio">Portfolio</a></li>
             </ul>
-            <div className="footer-socials">
+            <div className="footer-icons">
                 <a href="https://www.facebook.com/marlon.leon.710" target="_blank" 
                 rel="noopener noreferrer" className="anchor-facebook">
-                    <FontAwesomeIcon icon={faSquareFacebook} className="facebook-icon"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faSquareFacebook}></FontAwesomeIcon>
                 </a>
                 <a href="https://www.instagram.com/marlon.z32/" target="_blank" 
                 rel="noopener noreferrer" className="anchor-instagram">
-                    <FontAwesomeIcon icon={faInstagramSquare} className="instagram-icon"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon>
+                </a>
+                <a href="https://www.linkedin.com/in/marlon-leon/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                </a>
+                <a href="https://github.com/marlonleon01" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon>
                 </a>
             </div> 
         </footer>
