aa47a0b1182d4c73fbd52ffd1f7441cb4211f1af
Marlon Leon <ghostveene@gmail.com>
2023-01-12 16:48:33 -0500

    removed small imgs

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,8 @@ import "./Header.css"
 import GraduationSitting from "../../imgs/Graduation Sitting.avif"
 import Computer from "../../imgs/Computer.avif"
 import MiamiShirt from "../../imgs/Miami shirt.avif"
-import BeReal from "../../imgs/BeReal.avif"
-import Milo from "../../imgs/Milo.avif"
-import Munchkin from "../../imgs/Munchkin.avif"
 import MayaAndMe from "../../imgs/Maya and Me.avif"
-import LuciaAndMe from "../../imgs/Lucia and Me.avif"
+
 
 
 export function Header() {
@@ -18,28 +15,14 @@ export function Header() {
                 <h5 className="text-light">Front-End Developer</h5>
                 <div className="img-container">
                     <div className="img-group">
-                        <div className="small-img img">
-                            <img src={LuciaAndMe}></img>
-                        </div>
                         <div className="big-img img">
-                            <img src={BeReal}></img>
-                        </div>
-                        <div className="small-img img">
-                            <img src={Munchkin}></img>
+                            <img src={MayaAndMe}></img>
                         </div>
                         <div className="big-img img">
                             <img src={Computer}></img>
                         </div>
-                        
-                        <div className="small-img img">
-                            <img src={MayaAndMe}></img>
-                        </div>
                         <div className="big-img img">
                             <img src={MiamiShirt}></img>
-                        </div>
-                        
-                        <div className="small-img img">
-                            <img src={Milo}></img>
                         </div>
                         <div className="big-img img">
                             {/* Main Image */}
