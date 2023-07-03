0beb04647327ccf814224cb2f5db66525f955394
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-03 13:12:15 -0400

    added home navigation and removed social icons

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,24 +1,18 @@
 import "./Navbar.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faHouse} from "@fortawesome/free-solid-svg-icons"
 import {faUser} from "@fortawesome/free-solid-svg-icons"
 import {faBook} from "@fortawesome/free-solid-svg-icons"
 import {faAddressBook} from "@fortawesome/free-solid-svg-icons"
-import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
-import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
 
 export function Navbar() {
     return (
         <nav className="navbar" id="navbar">
-            {/* Add real website links */}
-            <div>
-                <a href="https://www.linkedin.com/in/marlon-leon/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} className="brand-icons" alt="linkedin icon"></FontAwesomeIcon>
-                </a>
-                <a href="https://github.com/marlonleon01" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithubSquare} className="brand-icons" alt="github icon"></FontAwesomeIcon>
-                </a>
-            </div>
             <div className="navbar-menu">
+                <a href="#home">
+                    <FontAwesomeIcon icon={faHouse} className="solid-icons"></FontAwesomeIcon>
+                    <span className="navbar-text">Home</span>
+                </a>
                 <a href="#about">
                     <FontAwesomeIcon icon={faUser} className="solid-icons"></FontAwesomeIcon>
                     <span className="navbar-text">About</span>
