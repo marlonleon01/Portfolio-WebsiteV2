829b072cb7ae5e905e626d9c1b73101096692aaf
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-05 13:59:42 -0400

    combined data again after figuring out how to render to get stagger effect

--- src/Components/Portfolio/dataPortfolio.js
+++ src/Components/Portfolio/dataPortfolio.js
@@ -3,7 +3,7 @@ import MovieWatchlist from "../../imgs/Movie Watchlist.webp"
 import Calculator from "../../imgs/Calculator.webp"
 import ColorSchemeGenerator from "../../imgs/Color Scheme Generator.webp"
 
-const dataPortfolioLeft = [
+const dataPortfolio = [
     {
         id: 1,
         image: TenziesGame,
@@ -15,20 +15,6 @@ const dataPortfolioLeft = [
     },
     {
         id: 2,
-        image: Calculator,
-        title: "Calculator",
-        github: "https://github.com/marlonleon01/Calculator",
-        live: "https://calculator.marlonleon.net/",
-        description: `A calculator that use JavaScript classes, built in functions,
-                        and data selectors.`
-
-    },
-    
-]
-
-const dataPortfolioRight = [
-    {
-        id: 1,
         image: MovieWatchlist,
         title: "Movie Watchlist",
         github: "https://github.com/marlonleon01/Movie-Watchlist",
@@ -37,7 +23,17 @@ const dataPortfolioRight = [
                         and OMDb API that uses localstorage to store the data.`
     },
     {
-        id: 2,
+        id: 3,
+        image: Calculator,
+        title: "Calculator",
+        github: "https://github.com/marlonleon01/Calculator",
+        live: "https://calculator.marlonleon.net/",
+        description: `A calculator that use JavaScript classes, built in functions,
+                        and data selectors.`
+
+    },
+    {
+        id: 4,
         image: ColorSchemeGenerator,
         title: "Color Scheme Generator",
         github: "https://github.com/marlonleon01/Color-Scheme-Generator",
@@ -46,4 +42,4 @@ const dataPortfolioRight = [
     },
 ]
 
-export {dataPortfolioLeft, dataPortfolioRight}
+export default dataPortfolio
