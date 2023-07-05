e8c1662d099f626bef2fff8abac2b2a9ad8ff8f4
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-05 16:07:07 -0400

    entire website is styled and looking good

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,12 +8,6 @@ import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
 export function Footer() {
     return (
         <footer>
-            <a href="#" className="footer-name">Marlon Leon</a>
-            <ul className="links">
-                <li><a href="#navbar">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-            </ul>
             <div className="footer-icons">
                 <a href="https://www.facebook.com/marlon.leon.710" target="_blank" 
                 rel="noopener noreferrer" className="anchor-facebook">
@@ -30,6 +24,7 @@ export function Footer() {
                     <FontAwesomeIcon icon={faGithubSquare} alt="github icon"></FontAwesomeIcon>
                 </a>
             </div> 
+            <a href="#" className="footer-name">Marlon Leon ©2023</a>
         </footer>
     )
 }
