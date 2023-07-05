90e430ba11b19cf8ce8ef50de38d417b4ee2844d
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-05 13:03:56 -0400

    added description

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,9 +1,7 @@
 import "./Portfolio.css"
 import TenziesGame from "../../imgs/Tenzies Game.webp"
 import MovieWatchlist from "../../imgs/Movie Watchlist.webp"
-import BlackJack from "../../imgs/BlackJack App.webp"
 import Calculator from "../../imgs/Calculator.webp"
-import MemeGenerator from "../../imgs/Meme Generator.webp"
 import ColorSchemeGenerator from "../../imgs/Color Scheme Generator.webp"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
@@ -14,28 +12,36 @@ const dataPortfolio = [
         image: TenziesGame,
         title: "Tenzies Game",
         github: "https://github.com/marlonleon01/Tenzies-Game",
-        live: "https://tenzies-game.marlonleon.net/"
+        live: "https://tenzies-game.marlonleon.net/",
+        description: `Tenzies game made with React.js that stores data 
+                        with state and functions with useEffect.`
     },
     {
         id: 2,
         image: MovieWatchlist,
         title: "Movie Watchlist",
         github: "https://github.com/marlonleon01/Movie-Watchlist",
-        live: "https://movie-watchlist.marlonleon.net/"
+        live: "https://movie-watchlist.marlonleon.net/",
+        description: `Movie watchlist app made with vanilla JavaScript 
+                        and OMDb API that uses localstorage to store the data.`
     },
     {
         id: 3,
         image: Calculator,
         title: "Calculator",
         github: "https://github.com/marlonleon01/Calculator",
-        live: "https://calculator.marlonleon.net/"
+        live: "https://calculator.marlonleon.net/",
+        description: `A calculator that use JavaScript classes, built in functions,
+                        and data selectors.`
+
     },
     {
         id: 4,
         image: ColorSchemeGenerator,
         title: "Color Scheme Generator",
         github: "https://github.com/marlonleon01/Color-Scheme-Generator",
-        live: "https://colorscheme-generator.marlonleon.net/"
+        live: "https://colorscheme-generator.marlonleon.net/",
+        description: `A color scheme generator that connects and fetches data from and API.`
     },
 ]
 
@@ -52,20 +58,23 @@ export function Portfolio() {
             </div>
             <div className="container portfolio-container">
                 {
-                    dataPortfolio.map(({id, image, title, github, live}) => {
+                    dataPortfolio.map(({id, image, title, description, github, live}) => {
                         return (
                             <article key={id} className="portfolio-item">
                                 <div className="portfolio-item-image">
                                     <img src={image} alt={title}></img>
                                 </div>
-                                <h3>{title}</h3>
-                                <div className="portfolio-item-btns">
-                                <a href={github} className="btn" target="_blank" rel="noopener noreferrer">
-                                    Github
-                                </a>
-                                <a href={live} className="btn" target="_blank" rel="noopener noreferrer">
-                                    Live View
-                                </a>
+                                <div className="portfolio-item-text">
+                                    <h3>{title}</h3>
+                                    <p>{description}</p>
+                                </div>
+                                <div  className="portfolio-item-btns">
+                                    <a href={github} className="btn" target="_blank" rel="noopener noreferrer">
+                                        Github
+                                    </a>
+                                    <a href={live} className="btn" target="_blank" rel="noopener noreferrer">
+                                        Live View
+                                    </a>
                                 </div>
                             </article>
                         )
