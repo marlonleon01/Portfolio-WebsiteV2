75ae1bc0afc2434bad1fd1189b6d12b234f65018
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-05 14:01:17 -0400

    rendering projects much cleaner and fixed stagger effect

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,49 +1,7 @@
 import "./Portfolio.css"
-import TenziesGame from "../../imgs/Tenzies Game.webp"
-import MovieWatchlist from "../../imgs/Movie Watchlist.webp"
-import Calculator from "../../imgs/Calculator.webp"
-import ColorSchemeGenerator from "../../imgs/Color Scheme Generator.webp"
+import dataPortfolio from "./dataPortfolio"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faGithubSquare} from "@fortawesome/free-brands-svg-icons"
-
-const dataPortfolio = [
-    {
-        id: 1,
-        image: TenziesGame,
-        title: "Tenzies Game",
-        github: "https://github.com/marlonleon01/Tenzies-Game",
-        live: "https://tenzies-game.marlonleon.net/",
-        description: `Tenzies game made with React.js that stores data 
-                        with state and functions with useEffect.`
-    },
-    {
-        id: 2,
-        image: MovieWatchlist,
-        title: "Movie Watchlist",
-        github: "https://github.com/marlonleon01/Movie-Watchlist",
-        live: "https://movie-watchlist.marlonleon.net/",
-        description: `Movie watchlist app made with vanilla JavaScript 
-                        and OMDb API that uses localstorage to store the data.`
-    },
-    {
-        id: 3,
-        image: Calculator,
-        title: "Calculator",
-        github: "https://github.com/marlonleon01/Calculator",
-        live: "https://calculator.marlonleon.net/",
-        description: `A calculator that use JavaScript classes, built in functions,
-                        and data selectors.`
-
-    },
-    {
-        id: 4,
-        image: ColorSchemeGenerator,
-        title: "Color Scheme Generator",
-        github: "https://github.com/marlonleon01/Color-Scheme-Generator",
-        live: "https://colorscheme-generator.marlonleon.net/",
-        description: `A color scheme generator that connects and fetches data from and API.`
-    },
-]
 
 export function Portfolio() {
     return (
@@ -58,26 +16,52 @@ export function Portfolio() {
             </div>
             <div className="container portfolio-container">
                 {
-                    dataPortfolio.map(({id, image, title, description, github, live}) => {
-                        return (
-                            <article key={id} className="portfolio-item">
-                                <div className="portfolio-item-image">
-                                    <img src={image} alt={title}></img>
-                                </div>
-                                <div className="portfolio-item-text">
-                                    <h3>{title}</h3>
-                                    <p>{description}</p>
-                                </div>
-                                <div  className="portfolio-item-btns">
-                                    <a href={github} className="btn" target="_blank" rel="noopener noreferrer">
-                                        Github
-                                    </a>
-                                    <a href={live} className="btn" target="_blank" rel="noopener noreferrer">
-                                        Live View
-                                    </a>
-                                </div>
-                            </article>
-                        )
+                    dataPortfolio.map(({id, image, title, description, github, live}, index) => {
+                        if (index % 2 === 0) {
+                            return (
+                                <article key={id} className="projects-container">
+                                    <div className={`project-img left`}>
+                                        <img src={image} alt={title}></img>
+                                    </div>
+                                    <div className="project-main-content">
+                                        <div className="project-text">
+                                            <h3>{title}</h3>
+                                            <p>{description}</p>
+                                        </div>
+                                        <div  className="project-btns">
+                                            <a href={github} className="btn" target="_blank" rel="noopener noreferrer">
+                                                GITHUB
+                                            </a>
+                                            <a href={live} className="btn" target="_blank" rel="noopener noreferrer">
+                                                LIVE VIEW
+                                            </a>
+                                        </div>
+                                    </div>
+                                </article>
+                            )
+                        } else {
+                            return (
+                                <article key={id} className="projects-container">
+                                    <div className="project-main-content">
+                                        <div className="project-text">
+                                            <h3>{title}</h3>
+                                            <p>{description}</p>
+                                        </div>
+                                        <div  className="project-btns">
+                                            <a href={github} className="btn" target="_blank" rel="noopener noreferrer">
+                                                GITHUB
+                                            </a>
+                                            <a href={live} className="btn" target="_blank" rel="noopener noreferrer">
+                                                LIVE VIEW
+                                            </a>
+                                        </div>
+                                    </div>                                    
+                                    <div className={`project-img right`}>
+                                        <img src={image} alt={title}></img>
+                                    </div>
+                                </article>
+                            )
+                        }
                     })
                 }
             </div>
